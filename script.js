0d7d919a52e15f243140d8c7b635e717365997b0
“Cecilie <ceci316e@stud.kea.dk>
2023-01-30 14:23:47 +0100

    øvelse 1

---
+++ script.js
@@ -0,0 +1,20 @@
+const minKnap1 = document.querySelector("#knapElement1");
+minKnap1.addEventListener("click", funktionDerKaldesVedKlik);
+function funktionDerKaldesVedKlik() {
+  console.log("Indeholder alkohol");
+}
+const minKnap2 = document.querySelector("#knapElement2");
+minKnap2.addEventListener("click", funktionDerKaldesVedKlik2);
+function funktionDerKaldesVedKlik2() {
+  console.log("Alkoholfri");
+}
+const minKnap3 = document.querySelector("#knapElement3");
+minKnap3.addEventListener("click", funktionDerKaldesVedKlik3);
+function funktionDerKaldesVedKlik3() {
+  console.log("Alkoholfri");
+}
+const minKnap4 = document.querySelector("#knapElement4");
+minKnap4.addEventListener("click", funktionDerKaldesVedKlik4);
+function funktionDerKaldesVedKlik4() {
+  console.log("Indeholder alkohol");
+}
